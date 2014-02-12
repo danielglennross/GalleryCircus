146e6ee22e2bc1715f0e26c9655cc901d137d41b
danielglennross <danielglennross@gmail.com>
2014-02-12 20:54:53 +0000

    updated repo

---
+++ 20131129/home.php
@@ -0,0 +1,335 @@
+<?php
+//start of file - Gallery Circus  - copyright 2013
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head> 
+	
+	<!-- google fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Loved+by+the+King' rel='stylesheet' type='text/css'>
+	<!-- end google fonts -->
+	
+	<!-- JQuery -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
+	<!-- end JQuery -->
+	
+	<title> 
+		Gallery Circus
+	</title> 
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+		<meta name="description" content="A unique and abrasive blues-rock outlet, Gallery Circus have quickly gained a strong reputation for delivering breathtakingly powerful live shows. Hailing from South Tyneside, Gallery Circus consists of 22 year-old identical twins Daniel (lead vocalist, guitar and piano) and Graeme Ross (drums and backing vocals).
+		Having played in bands from the tender age of five, Gallery Circus began to take on its current form when the brothers relocated to Chicagos hipster haven Wicker Park two years ago, in a bid to write new music, and thrash together something extraordinary. The brothers became known for throwing DIY gigs around the city and quickly built up a buzz. 
+		Helping to spark an anti-garage/ street pop scene with several like-minded bands - Daniel and Graeme took to playing upon rooftops, illegal make-shift venues and public landmarks amongst the usual routine spots. Despite having no instruments of their own (not to mention getting evicted from their apartment after two months, due to their penchant for parties), 
+		the pair managed to scrape together equipment from like-minded musicians and acquired quite a following in the iconic city. Having soaked up their new-found Americana influences, Gallery Circus returned to the UK last year, and with their white-hot, screeching blues, and reinforced their reputation as one of the most entertaining and fearsome live performers on the circuit.
+		Gallery Circus is designed to be a live thing, says Graeme. Were all about spontaneity and raw passion. Well never use drum loops or samples. Live and on record, our songs capture what can only be achieved by two people, even if that means playing guitar and piano simultaneously. The band managed to retain their in-your-face live sound when they released 
+		their debut EP in August last year to rapturous applause. Having relocated back to the states in 2012, this time to Americas most avant-garde city, San Francisco, the twins insist they are taking a strong influence from their surroundings which will be evident when they return home at the end of the year." /> 
+        
+		<meta name="keywords" content="Gallery Circus, Band, Music, Garage, Rock, Daniel Ross, Graeme Ross, Hipsters" /> 
+
+		<meta name='robots' content='all, index, follow' /> 
+        <meta name='rating' content='general' /> 
+        <meta name='distribution' content="Gallery Circus" /> 
+        <meta name='revisit-after' content='1 days' />
+        <meta name='language' content='en' />
+        <meta name='copyright' content="Gallery Circus" />
+        <meta name='alias' content='http://www.gallerycircus.co.uk/' />
+        <meta name='author' content="Gallery Circus" />
+
+		<meta http-equiv='expires' content='0' /> 
+		<meta http-equiv='pragma' content='no-cache' /> 
+		<meta http-equiv='cache-control' content='no-cache' /> 
+
+		<!-- all links relative to the domain - may need changed when addondomain created -->
+		<!-- Favicon -->
+		<link rel="icon" type="image/ico" href="/img/favicon.ico" />
+		<!-- end Favicon -->
+		
+		<!-- css -->
+		<LINK href="/css/site.css" rel="stylesheet" type="text/css">
+		<!-- end css -->
+
+</head>
+
+<body>
+
+<!-- header / nav -->
+<div id="nav" class="headerFooter">
+	<div id="nav2">
+		
+		<div id="leftNav">
+			<div id="logo">
+				<a href="http://www.gallerycircus.co.uk"><img id="tornado" src="/img/GCLogo.png"></a>
+				<a id="linkSee" href="javascript:"><img id="arrow" src="/img/arrowdown.png"></a>
+			</div>
+			<div id="pageLinks">
+				<ul>
+					<li class="linkElement"><a id="linkShows" href="#shows" class="shakeable">SHOWS</a></li>
+					<li class="linkElement"><a id="linkMedia" href="#music" class="shakeable">MUSIC</a></li>
+					<li class="linkElement"><a id="linkContact" href="#contact" class="shakeable">CONTACT</a></li>
+				</ul>
+			</div>
+		</div>
+		
+		<div id="rightNav">
+			<div id="socialTab">
+				<a class="socialLink" alt="Gallery Circus on Facebook" target="_blank" href="http://www.facebook.com/gallerycircus"><img class="socialIcon shakeable" src="/img/Facebook.png"></a>
+				<a class="socialLink" alt="Gallery Circus on Twitter" target="_blank" href="http://www.twitter.com/gallerycircus"><img class="socialIcon shakeable" src="/img/Twitter.png"></a>
+				<a class="socialLink" alt="Gallery Circus on YouTube" target="_blank" href="http://www.youtube.com/gallerycircus"><img class="socialIcon shakeable" src="/img/Youtube.png"></a>
+				<a class="socialLink" alt="Gallery Circus on Tumblr" target="_blank" href="http://sanfrancircus.tumblr.com"><img class="socialIcon shakeable" src="/img/Tumblr.png"></a>
+			</div>
+		</div>
+		
+	</div>
+</div>
+<!-- end header / nav -->
+	
+<!-- main content -->	
+<div id="mainContainer">
+	<div id="polaroidsTopContainer" class="polaroid">		
+		<div id="polaroidsTop">
+			<img id="polaroidsTopImg" src="/img/GCSite.png">
+		</div>	
+	</div>
+
+	<div id="music">
+		<div id="media1" class="media polaroidFade">	
+			<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F21723346"></iframe>	
+			<iframe width="560" height="315" src="http://www.youtube.com/embed/dwZr6Q3EhkE" frameborder="0" allowfullscreen></iframe>
+			<a href="http://sanfrancircus.tumblr.com" class="shakeable" target="_blank">In 10, and other tales of stateside adventures.</a>
+		</div>
+	
+		<div id="media2" class="media polaroidFade">	
+			<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F21726953"></iframe>	
+			<iframe width="560" height="315" src="http://www.youtube.com/embed/qNNUgxZGKC4" frameborder="0" allowfullscreen></iframe>
+			<a class="shakeable" target="_blank" href="http://www.soundcloud.com/gallerycircus">Stream the debut Gallery Circus E.P on SoundCloud.</a>
+		</div>
+	</div>
+	
+	<div class="clear"></div>
+	
+	<div id="shows">
+	
+		<div id="leftGig">
+			<img src="/img/gigList.png">
+		</div>
+	
+		<div id="innerGigContainer" class="polaroidFade">
+			<table width="100%">			
+				<thead><tr><th width="22%">Date</th><th width="60%">Venue</th><th width="18%">Event</th></tr></thead>
+				<tbody>
+					<!--<tr><td align="left" class="tblDate">&nbsp;March 31</td><td>Think Tank, B>E>A>K Easter Party, Newc</td><td><a href="https://www.facebook.com/events/136619866508813/" target="_blank"><img class="shakeable" src="/img/facebookGig.png"></a></td></tr>
+					<tr><td align="left" class="tblDate">&nbsp;April 6</td><td>The Hollywood and Vine, NARC magazine, Newc</td><td><a href="https://www.facebook.com/pages/The-Boiler-Shop-Steamer/420227184736159?ref=ts&fref=ts" target="_blank"><img class="shakeable" src="/img/facebookGig.png"></a></td></tr>
+					<tr><td align="left" class="tblDate">&nbsp;April 9</td><td>The Garage Sessions</td><td><a href="https://www.facebook.com/garagerehearsals" target="_blank"><img class="shakeable" src="/img/facebookGig.png"></a></td></tr>
+					<tr><td align="left" class="tblDate">&nbsp;TBA...</td><td>Summer festivals announced soon</td><td><a href="http://www.facebook.com/gallerycircus" target="_blank"><img class="shakeable" src="/img/facebookGig.png"></a></td></tr>
+					<tr><td  align="left" class="tblDate">May 14th</td><td>Think Tank, Newcastle with band</td><td><a href="http://www.gallerycircus.co.uk"><img class="shakeable" src="/img/facebookGig.png"></a></td></tr>
+					<tr><td  align="left" class="tblDate">May 14th</td><td>Think Tank, Newcastle with band</td><td><a href="http://www.gallerycircus.co.uk"><img class="shakeable" src="/img/facebookGig.png"></a></td></tr>
+					<tr><td align="left" class="tblDate">&nbsp;JUNE 17</td><td>THE CLUNY, NEWC</td><td><a href="https://www.facebook.com/events/334793946647231" target="_blank"><img class="shakeable" src="/img/facebookGig.png"></a></td></tr>
+					<tr><td align="left" class="tblDate">&nbsp;AUGUST 4</td><td>BERNICIA RISING 2013</td><td><a href="https://www.facebook.com/pages/Bernicia-Festival/229348307209048" target="_blank"><img class="shakeable" src="/img/facebookGig.png"></a></td></tr>
+					<tr><td align="left" class="tblDate">&nbsp;AUGUST 25</td><td>GALTRES FEST, HELMSLEY</td><td><a href="https://www.facebook.com/galtresfestival.org.uk" target="_blank"><img class="shakeable" src="/img/facebookGig.png"></a></td></tr>
+					<tr><td align="left" class="tblDate">&nbsp;AUGUST 29</td><td>THE CLUNY 2, NEWC</td><td><a href="https://www.facebook.com/events/144896692374064" target="_blank"><img class="shakeable" src="/img/facebookGig.png"></a></td></tr>-->
+					<tr><td align="left" class="tblDate">&nbsp;October 12</td><td>THINK TANK (supporting Spector), NEWC</td><td><a href="https://www.facebook.com/events/210867045704254" target="_blank"><img class="shakeable" src="/img/facebookGig.png"></a></td></tr>
+				</tbody>
+			</table>
+			
+			<div id="innerGigContainerText">
+				Music available to purchase at:
+				<a href="http://www.amazon.co.uk/s/ref=ntt_srch_drd_B005WRXK3U?ie=UTF8&field-keywords=Gallery%20Circus&index=digital-music&search-type=ss" class="shakeable" target="_blank">Amazon</a>&nbsp;|
+				<a href="http://gallerycircus.bandcamp.com/" class="shakeable" target="_blank">Bandcamp</a>		
+			</div>
+			
+		</div>
+	
+	</div>
+	
+	<div class="clear"></div>
+	
+	<div id="polaroidsBottomContainer" class="polaroid">	
+		<div id="polaroidsBottom">
+			<img id="polaroidsBottomImg" src="/img/GCSite2.png">
+		</div>	
+	</div>
+	
+	<div id="newContact">
+		<img id="newContactImg" src="/img/contact.png">
+	</div>
+	
+	</div>
+</div>
+<!-- end main content -->
+
+<!-- contact footer -->
+<footer>
+
+	<div id="contact" class="headerFooter">
+		<div id="footerText">
+			Design & Developed by Gallery Circus 2013 
+			&nbsp;&nbsp;
+			<a href="http://www.amazon.co.uk/s/ref=ntt_srch_drd_B005WRXK3U?ie=UTF8&field-keywords=Gallery%20Circus&index=digital-music&search-type=ss" class="shakeable" target="_blank">Amazon</a>&nbsp;|
+			<a href="http://gallerycircus.bandcamp.com/" class="shakeable" target="_blank">Bandcamp</a>&nbsp;|
+			<a class="shakeable" target="_blank" href="http://www.soundcloud.com/gallerycircus">SoundCloud</a>&nbsp;|
+			<a class="shakeable" target="_blank" href="http://www.vimeo.com/gallerycircus">Vimeo</a>
+		</div>
+	</div>
+
+</footer>
+<!-- end contact footer -->
+
+<script>
+
+
+$(function() {
+
+	GalleryCircus.AllMedia = $("iframe[src^='http://www.youtube.com'], iframe[src^='https://w.soundcloud.com']");
+	GalleryCircus.FluidContainer = $(".media");
+	GalleryCircus.Init();
+
+});
+
+var GalleryCircus = {
+
+	AllMedia: null, // store all the media to be resized
+	
+	FluidContainer: null, // store fluid container
+	
+	CauseRepaintsOn: $("#contact, #innerGigContainer table, #innerGigContainerText, .media, .headerFooter"), // elements that need text repainted for Chrome
+	
+	Init: function() {
+	
+		// save all aspect ratios of media, and handler for resize window
+		// then make all youtube links have 'wmode=transparent' at end of link (always display under nav)
+		// apply style to nav links
+		// move to furtherst polaroid
+		GalleryCircus.SaveAspect();
+		GalleryCircus.ReSize();
+		GalleryCircus.MakeYoutubeVideosTransparent();
+		GalleryCircus.ShakeIcon();
+		GalleryCircus.SlideToLink();
+		GalleryCircus.LinkSeeNav();
+		GalleryCircus.Scroll();
+		
+	},
+	
+	SlideToLink: function() {
+		// foreach a link with slide
+		$('a[href*=#]').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				GalleryCircus.Slide($(this.hash));
+			}
+		}); 
+	},
+	
+	Slide: function(hash) {
+		// slide to div
+		var $target = hash;
+		$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+		if ($target.length) {
+			var targetOffset = $target.offset().top;
+			$('html,body').animate({scrollTop: targetOffset - 100}, 600);
+			return false;
+		}
+	},
+	
+	Scroll: function() {
+		$(window).bind('scroll', function() {
+			var furthestDiv = GalleryCircus.CalcPolaroidFurthestDiv();
+			
+			var arrowDown = "<img id=\"arrow\" src=\"/img/arrowdown.png\">";
+			var arrowUp = "<img id=\"arrow\" src=\"/img/arrowup.png\">";
+			
+			if (furthestDiv == "polaroidsTopContainer") $("#linkSee").html(arrowUp); else $("#linkSee").html(arrowDown);
+		});
+	},
+	
+	LinkSeeNav: function() {
+		$("#linkSee").click(function() {
+			var furthestDiv = GalleryCircus.CalcPolaroidFurthestDiv(); 
+			GalleryCircus.Slide($("#" + furthestDiv));
+		});
+	},
+	
+	CalcPolaroidFurthestDiv: function() {
+		var lowestPos = null;
+		var furthestDiv = "";
+		$('.polaroid').each(function() {
+			var polaroid = $(this);
+			var position = polaroid.position().top - $(window).scrollTop();
+			
+			if (position < 0) position = position * -1;
+			
+			if (lowestPos == null || position > lowestPos)
+			{
+				lowestPos = position;
+				furthestDiv = polaroid.attr("id");
+			}
+		}); 
+		return furthestDiv;
+	},
+	
+	SaveAspect: function() {
+		// Figure out and save aspect ratio for each video
+		GalleryCircus.AllMedia.each(function() {
+
+			$(this)
+				.data('aspectRatio', this.height / this.width)
+				
+				// and remove the hard coded width/height
+				.removeAttr('height')
+				.removeAttr('width');
+
+		});
+	},
+	
+	ReSize: function() {
+		// When the window is resized
+		// debounce this?
+		$(window).resize(function() {
+
+			GalleryCircus.FixChromeTextBugOnWindowResize();
+		
+			var newWidth = GalleryCircus.FluidContainer.width();
+			
+			// Resize all videos according to their own aspect ratio
+			GalleryCircus.AllMedia.each(function() {
+
+				var $el = $(this);
+				$el
+					.width(newWidth)
+					.height(newWidth * $el.data('aspectRatio'));
+
+			});
+
+		// Kick off one resize to fix all videos on page load
+		}).resize();
+	},
+	
+	FixChromeTextBugOnWindowResize: function() {
+		// re-paint (add a pointless css style) each of the following elemts to re-render viewport text size
+		GalleryCircus.CauseRepaintsOn.css("border","none");
+	},
+	
+	MakeYoutubeVideosTransparent: function() {
+		$("[src^='http://www.youtube.com']").each(function() {
+           var url = $(this).attr("src");
+           var char = "?";
+           
+		   if (url.indexOf("?") != -1)
+                  var char = "&";
+          
+            $(this).attr("src",url+char+"wmode=transparent");
+		});
+	},
+	
+	ShakeIcon: function() {
+		$(".shakeable").mouseover(function () {
+			$(this).effect("pulsate", { times:1 }, 500);
+		});
+	}
+}
+
+</script>
+
+</body>
+</html>
+
---
+++ liveCode/shows.php
@@ -0,0 +1,312 @@
+<?php
+//start of file - Gallery Circus  - copyright 2013
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> 
+			Gallery Circus Supercell Launch
+		</title> 
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+		<meta name="description" content="A unique and abrasive blues-rock outlet, Gallery Circus have quickly gained a strong reputation for delivering breathtakingly powerful live shows. Hailing from South Tyneside, Gallery Circus consists of 22 year-old identical twins Daniel (lead vocalist, guitar and piano) and Graeme Ross (drums and backing vocals).
+		Having played in bands from the tender age of five, Gallery Circus began to take on its current form when the brothers relocated to Chicagos hipster haven Wicker Park two years ago, in a bid to write new music, and thrash together something extraordinary. The brothers became known for throwing DIY gigs around the city and quickly built up a buzz. 
+		Helping to spark an anti-garage/ street pop scene with several like-minded bands - Daniel and Graeme took to playing upon rooftops, illegal make-shift venues and public landmarks amongst the usual routine spots. Despite having no instruments of their own (not to mention getting evicted from their apartment after two months, due to their penchant for parties), 
+		the pair managed to scrape together equipment from like-minded musicians and acquired quite a following in the iconic city. Having soaked up their new-found Americana influences, Gallery Circus returned to the UK last year, and with their white-hot, screeching blues, and reinforced their reputation as one of the most entertaining and fearsome live performers on the circuit.
+		Gallery Circus is designed to be a live thing, says Graeme. Were all about spontaneity and raw passion. Well never use drum loops or samples. Live and on record, our songs capture what can only be achieved by two people, even if that means playing guitar and piano simultaneously. The band managed to retain their in-your-face live sound when they released 
+		their debut EP in August last year to rapturous applause. Having relocated back to the states in 2012, this time to Americas most avant-garde city, San Francisco, the twins insist they are taking a strong influence from their surroundings which will be evident when they return home at the end of the year." /> 
+        
+		<meta name="keywords" content="Gallery Circus, Band, Music, Garage, Rock, Daniel Ross, Graeme Ross, Hipsters, Supercell, Single Launch" /> 
+
+		<meta name='robots' content='all, index, follow' /> 
+        <meta name='rating' content='general' /> 
+        <meta name='distribution' content="Gallery Circus" /> 
+        <meta name='revisit-after' content='1 days' />
+        <meta name='language' content='en' />
+        <meta name='copyright' content="Gallery Circus" />
+        <meta name='alias' content='http://www.gallerycircus.co.uk/' />
+        <meta name='author' content="Gallery Circus" />
+
+		<meta http-equiv='expires' content='0' /> 
+		<meta http-equiv='pragma' content='no-cache' /> 
+		<meta http-equiv='cache-control' content='no-cache' /> 
+		
+		<link rel="icon" type="image/ico" href="/img/favicon.ico" />
+	
+		<style>
+		
+			html {
+				height: 100%;
+			}
+
+			body {
+				margin:0px;
+				padding:0px;
+				list-style:none;
+				background-color:#000;
+				
+				font-family: 'Permanent Marker', cursive;
+				font-size: 2.5vw;
+				font-size: 2.5vh;
+				font-size: 2.5vmin;
+				
+			}
+				
+			a {
+				text-decoration:none;
+				color: #000;
+				
+				transition: 0.2s all ease-in;
+				-webkit-transition: 0.2s all ease-in;
+				-moz-transition: 0.2s all ease-in;
+			}
+			a:hover {
+				text-decoration:none;
+				color: #cc0033;
+			}
+			a:active {
+				text-decoration:none;
+				color: #000;
+			}
+
+			a img {
+				border:none;
+				outline:none;
+			}
+
+			.clear {
+				clear:both;
+			}
+
+			#socialTab{
+				height:100%;
+				width:90%;
+				float:right;
+			}
+
+			.socialLink {
+				height:100%
+			}
+
+			.socialIcon {
+				height:75%;
+				width:16%;
+				margin-top:1%;
+				
+				background-color: rgba(233, 234, 230, 0.4);
+
+				/* outline: 0;
+				outline: thin dotted \9;
+				IE6-9 */
+
+				-webkit-box-shadow: inset 0 0.2% 0.2% rgba(0, 0, 0, 0.075), 0 0 0.2% rgba(233, 234, 230, 1);
+				   -moz-box-shadow: inset 0 0.2% 0.2% rgba(0, 0, 0, 0.075), 0 0 0.2% rgba(233, 234, 230, 1);
+						box-shadow: inset 0 0.2% 0.2% rgba(0, 0, 0, 0.075), 0 0 0.2% rgba(233, 234, 230, 1);
+			}
+
+			.headerFooter {
+				width:100%;
+				position:fixed;
+				z-index: 100;
+				height: 6%;
+				border-bottom: 3px solid #000;
+				
+				background: url("/img/navSite.jpg") repeat scroll center bottom transparent;
+				zoom: 1;
+				filter: alpha(opacity=90);
+				opacity: 0.9;
+				-moz-box-shadow:    0px 0px 1% 2% #000; /* ccc */
+				-webkit-box-shadow: 0px 0px 1% 2% #000;
+				box-shadow:         0px 0px 1% 2% #000;	
+			}
+
+			#nav2 {
+				width:95%;
+				margin: 0 auto;
+			}
+
+			#leftNav {
+				float:left;
+				width: 80%;
+			}
+
+			#rightNav {
+				float:right;
+				width: 20%;
+				padding-top:0.1%;
+				height:100%;
+			}
+
+			#logoTornado {
+				width:18%;
+				float:left;
+			}
+			
+			#poster {
+				width:100%;
+			}
+
+			#tornadoNew {
+				margin-top:1%;
+				width:85%;
+			}
+
+			#pageLinks {
+				float:right;
+				width:45%;
+			}
+			
+			#linkShows{
+				
+				font-size: 4vw;
+				font-size: 4vh;
+				font-size: 4vmin;
+				text-transform:uppercase;
+			}
+
+			#pageLinks ul {
+				margin: 0px;
+				padding: 0px;
+			}
+
+			#pageLinks ul li {
+				display: inline; 
+				margin-right: 0.5%;
+			}
+			
+			#supercellVideo {
+				height: 100%;
+				position: relative;
+				margin: 0 auto;
+				padding-bottom:3%;
+				margin-top:-5%;
+				width: 93%;
+			}
+
+			/* Media Queries */
+
+			/* Smartphones (portrait and landscape) ----------- */
+			@media only screen 
+			and (min-device-width : 320px) 
+			and (max-device-width : 480px) {
+				
+			}
+
+			/* iPads (portrait and landscape) ----------- */
+			@media only screen 
+			and (min-device-width : 768px) 
+			and (max-device-width : 1024px) {
+				
+			}
+
+		</style>
+
+	</head>
+
+	<body>
+
+		<!-- header / nav -->
+		<div class="headerFooter">
+			<div id="nav2">
+				
+				<div id="leftNav">
+					<div id="logoTornado">
+						<a href="http://www.gallerycircus.co.uk"><img id="tornadoNew" src="/img/GCLogoRed.png"></a>
+					</div>
+					<div id="pageLinks">
+						<ul>
+							<li class="linkElement"><a id="linkShows" href="http://www.seetickets.com/Event/gallery-circus/think-tank-/757300" class="shakeable">Buy Tickets</a></li>
+						</ul>
+					</div>
+				</div>
+				
+				<div id="rightNav">
+					<div id="socialTab">
+						<a class="socialLink" alt="Gallery Circus on Facebook" target="_blank" href="http://www.facebook.com/gallerycircus"><img class="socialIcon shakeable" src="/img/Facebook.png"></a>
+						<a class="socialLink" alt="Gallery Circus on Twitter" target="_blank" href="http://www.twitter.com/gallerycircus"><img class="socialIcon shakeable" src="/img/Twitter.png"></a>
+						<a class="socialLink" alt="Gallery Circus on YouTube" target="_blank" href="http://www.youtube.com/gallerycircus"><img class="socialIcon shakeable" src="/img/Youtube.png"></a>
+						<a class="socialLink" alt="Gallery Circus on Tumblr" target="_blank" href="http://sanfrancircus.tumblr.com"><img class="socialIcon shakeable" src="/img/Tumblr.png"></a>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<!-- end header / nav -->
+	
+		
+		<div id="Supercell" style="width:100%;">
+		
+			<img id="poster" src="/img/singleLaunchPoster.png">
+			
+			<div id="supercellVideo" class="media">
+				<iframe width="1280" height="720" frameborder="0" allowfullscreen="" src="http://www.youtube.com/embed/mTdQ99G1n8A?wmode=transparent"></iframe>
+			</div>
+				
+		</div>
+		
+		<!-- JQuery -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
+		<!-- end JQuery -->
+	
+		<link href='http://fonts.googleapis.com/css?family=Permanent+Marker' rel='stylesheet' type='text/css'>
+		
+		<script type="text/javascript">
+		
+		$(function() {
+
+			GalleryCircus.AllMedia = $("iframe[src^='http://www.youtube.com']");
+			GalleryCircus.FluidContainer = $(".media");
+			GalleryCircus.Init();
+
+		});
+		
+		var GalleryCircus = {
+		
+				AllMedia: null, // store all the media to be resized
+				
+				FluidContainer: null, // store fluid container
+				
+				CauseRepaintsOn: $("#linkShows"), // elements that need text repainted for Chrome
+				
+				Init: function() {
+				
+					GalleryCircus.ReSize();
+				
+				},
+		
+				ReSize: function() {
+				// When the window is resized
+				// debounce this?
+				$(window).resize(function() {
+
+					GalleryCircus.FixChromeTextBugOnWindowResize();
+				
+					var newWidth = GalleryCircus.FluidContainer.width();
+					
+					// Resize all videos according to their own aspect ratio
+					GalleryCircus.AllMedia.each(function() {
+
+						var $el = $(this);
+						$el
+							.width(newWidth)
+							.height(newWidth * $el.data('aspectRatio'));
+
+					});
+
+				// Kick off one resize to fix all videos on page load
+				}).resize();
+			},
+			
+			FixChromeTextBugOnWindowResize: function() {
+				// re-paint (add a pointless css style) each of the following elemts to re-render viewport text size
+				GalleryCircus.CauseRepaintsOn.css("border","none");
+			}
+		}
+		
+		</script>
+		
+	</body>
+</html>
